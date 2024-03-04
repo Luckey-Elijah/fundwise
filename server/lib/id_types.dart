a20d0fca5043db8277a03a19be8721378896d2c4
Elijah Luckey <luckeyelijah112@gmail.com>
2024-03-03 21:27:18 -0500

    turns out i don't need it (yet?)

--- server/lib/id_types.dart
+++
@@ -1,7 +0,0 @@
-extension type BudgetId(String id) {}
-extension type AccountId(String id) {}
-extension type UserId(String id) {}
-extension type CategoryId(String id) {}
-extension type PayeeId(String id) {}
-extension type TransactionId(String id) {}
-extension type FlagId(String id) {}
