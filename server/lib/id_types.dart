e43a291182e5560bb46757280eae1713678e577c
Elijah Luckey <luckeyelijah112@gmail.com>
2024-03-03 21:19:18 -0500

    getting inline class

---
+++ server/lib/id_types.dart
@@ -0,0 +1,7 @@
+extension type BudgetId(String id) {}
+extension type AccountId(String id) {}
+extension type UserId(String id) {}
+extension type CategoryId(String id) {}
+extension type PayeeId(String id) {}
+extension type TransactionId(String id) {}
+extension type FlagId(String id) {}
