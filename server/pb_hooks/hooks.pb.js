b0345d4d49a94e2dcb1271ba8eecbc0a1f20d495
Elijah Luckey <luckeyelijah112@gmail.com>
2024-03-18 09:19:57 -0400

    Hooks

--- server/pb_hooks/hooks.pb.js
+++ server/pb_hooks/hooks.pb.js
@@ -1,13 +1,1 @@
 /// <reference path="../pb_data/types.d.ts" />
-
-onRecordBeforeCreateRequest((e) => {
-  console.log(e.httpContext);
-  console.log(e.record);
-  console.log(e.uploadedFiles);
-});
-
-onRecordBeforeCreateRequest((e) => {
-  console.log(e.httpContext);
-  console.log(e.record);
-  console.log(e.uploadedFiles);
-});
