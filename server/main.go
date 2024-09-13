128309e025a0780d0059a6c6f5aef570af491374
Elijah Luckey <luckeyelijah112@gmail.com>
2024-09-13 16:23:04 -0400

    some more go

--- server/main.go
+++ server/main.go
@@ -2,32 +2,19 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
-    app := pocketbase.New()
+	app := pocketbase.New()
 
-    // serves static files from the provided public dir (if exists)
-    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-        e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
-        return nil
-    })
+	app.OnRecordBeforeCreateRequest("users").Add(func(e *core.RecordCreateEvent) error {
+		return nil
+	})
 
-		app.OnModelAfterCreate("users").Add(func(e *core.ModelEvent) error {
-			log.Print(e.Model.GetId())
-			log.Print(e.Tags())
-			e.BaseModelEvent.Tags()
-			// prints name of user
-			e.Model.TableName()
-			return nil;
-		})
-
-    if err := app.Start(); err != nil {
-        log.Fatal(err)
-    }
+	if err := app.Start(); err != nil {
+			log.Fatal(err)
+	}
 }
